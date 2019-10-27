c43cae6ad48f035b2c74b9bf20ec2cfa324f8c5d
Rajat Arora <rajat@rajatarora.info>
2019-10-27 22:13:23 +0530

    Spring Boot Starter project (Gradle / Kotlin)

---
+++ src/main/kotlin/app/rupaiya/rupaiyaservices/RupaiyaServicesApplication.kt
@@ -0,0 +1,11 @@
+package app.rupaiya.rupaiyaservices
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RupaiyaServicesApplication
+
+fun main(args: Array<String>) {
+	runApplication<RupaiyaServicesApplication>(*args)
+}
---
+++ src/test/kotlin/app/rupaiya/rupaiyaservices/RupaiyaServicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.rupaiya.rupaiyaservices
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RupaiyaServicesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
